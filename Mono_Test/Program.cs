4799a8f1fa4bb574f090ce4aab7a6fe51aabea14
ForrestCai <caizi-wg@163.com>
2017-08-16 10:57:30 +0800

    TopShelf

--- Mono_Test/Program.cs
+++ Mono_Test/Program.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.Owin.Hosting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using Topshelf;
 
 namespace Mono_Test
 {
@@ -20,15 +16,38 @@ namespace Mono_Test
             //Task.Factory.StartNew(() => { Console.WriteLine("Hello Word!" + Thread.CurrentThread.ManagedThreadId); });
 
             //Console.ReadKey();
-            string baseAddress = "http://localhost:9000/";
-            var runtime = Type.GetType("Mono.Runtime") != null ? "Mono" : "MS DotNet";
+            //string baseAddress = "http://*:9000/";
+            //var runtime = Type.GetType("Mono.Runtime") != null ? "Mono" : "MS DotNet";
 
-            // Start OWIN host 
-            using (WebApp.Start<Startup>(url: baseAddress))
+            ////// Start OWIN host 
+            //using (WebApp.Start<Startup>(url: baseAddress))
+            //{
+            //    Console.WriteLine("Service is started on " + runtime + " " + baseAddress);
+            //    //Console.ReadLine();
+            //    while (true)
+            //    {
+            //        Thread.Sleep(1);
+            //    }
+            //}
+
+            //Thread.Sleep(10000);
+
+            HostFactory.Run(c =>
             {
-                Console.WriteLine("Service is started on " + runtime);
-                Console.ReadLine();
-            }
+                c.SetServiceName("MonoTestServices");
+                c.SetDisplayName("MonoTestServices");
+                c.SetDescription("MonoTestServices");
+
+                c.Service<TopshelfService>(s =>
+                {
+                    s.ConstructUsing(b => new TopshelfService());
+                    s.WhenStarted(o => o.Start());
+                    s.WhenStopped(o => o.Stop());
+                });
+                c.UseLinuxIfAvailable();
+                c.RunAsLocalService();
+            });
+
         }
     }
 }
---
+++ Mono_Test/TopshelfService.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+
+namespace Mono_Test
+{
+    public class TopshelfService
+    {
+        public void Start()
+        {
+            string baseAddress = "http://*:9000/";
+            //var runtime = Type.GetType("Mono.Runtime") != null ? "Mono" : "MS DotNet";
+            //Console.WriteLine("Service is started on " + runtime + " " + baseAddress);
+            WebApp.Start<Startup>(url: baseAddress);
+        }
+
+        public void Stop()
+        {
+        }
+    }
+}
